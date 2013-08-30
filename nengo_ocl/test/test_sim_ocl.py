2cd3de85d874790124265f440930a07a8a377428
James Bergstra <james.bergstra@gmail.com>
2013-08-30 11:22:56 -0400

    fixed test_sim_ocl imports

--- nengo_ocl/test/test_sim_ocl.py
+++ nengo_ocl/test/test_sim_ocl.py
@@ -1,8 +1,19 @@
-try:
-    # For Python <=2.6
-    import unittest2 as unittest
-except ImportError:
-    import unittest
+"""
+Black-box testing of the sim_npy Simulator.
+
+TestCase classes are added automatically from
+nengo.tests.helpers.simulator_test_cases, but
+you can still run individual test files like this:
+
+    nosetests -sv test/test_sim_npy.py:TestSimulator.test_simple_direct_mode
+
+"""
+
+from nengo_ocl.tricky_imports import unittest
+
+import nengo.tests.helpers
+
+from nengo_ocl import sim_ocl
 
 import pyopencl as cl
 from nengo.tests.helpers import load_nengo_tests, simulator_test_suite
@@ -41,6 +52,26 @@ try:
 
 except ImportError:
     pass
+
+
+ctx = cl.create_some_context()
+
+def simulator_allocator(*args, **kwargs):
+    rval = sim_ocl.Simulator(ctx, *args, **kwargs)
+    rval.plan_all()
+    return rval
+
+load_tests = nengo.tests.helpers.load_nengo_tests(simulator_allocator)
+
+for foo in load_tests(None, None, None):
+    class MyCLS(foo.__class__):
+        def Simulator(self, model):
+            return simulator_allocator(model)
+    globals()[foo.__class__.__name__] = MyCLS
+    MyCLS.__name__ = foo.__class__.__name__
+    del MyCLS
+    del foo
+del load_tests
 
 
 if __name__ == "__main__":
