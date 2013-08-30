6271e5445c6782259e33817e0ef08d00db58b54b
James Bergstra <james.bergstra@gmail.com>
2013-08-30 11:22:56 -0400

    fixed test_sim_ocl imports

--- nengo_ocl/test/test_sim_ocl.py
+++ nengo_ocl/test/test_sim_ocl.py
@@ -16,43 +16,6 @@ import nengo.tests.helpers
 from nengo_ocl import sim_ocl
 
 import pyopencl as cl
-from nengo.tests.helpers import load_nengo_tests, simulator_test_suite
-
-from nengo_ocl import sim_ocl
-
-
-ctx = cl.create_some_context(interactive=False)
-
-from nengo_ocl import sim_ocl
-
-def simulator_allocator(*args, **kwargs):
-    rval = sim_ocl.Simulator(ctx, *args, **kwargs)
-    rval.alloc_all()
-    rval.plan_all()
-    return rval
-
-load_tests = load_nengo_tests(simulator_allocator)
-
-try:
-    import nose  # If we have nose, maybe we're doing nosetests.
-
-    # Stop some functions from running automatically
-    load_nengo_tests.__test__ = False
-    load_tests.__test__ = False
-    simulator_test_suite.__test__ = False
-
-    # unittest won't try to run this, but nose will
-    def test_nengo():
-        nengo_suite = simulator_test_suite(simulator_allocator)
-
-        # Unfortunately, this makes us run nose twice,
-        # which gives two sets of results.
-        # I don't know a way around this.
-        assert nose.run(suite=nengo_suite, exit=False)
-
-except ImportError:
-    pass
-
 
 ctx = cl.create_some_context()
 
